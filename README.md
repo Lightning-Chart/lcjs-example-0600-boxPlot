5025fd4c3ce14c4c3d810ebeb8d337de30d88899
Ilkka Kuosmanen <ilkka.kuosmanen@arction.com>
2019-08-05 13:42:19 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,85 @@
+# Box and Whiskers Chart
+
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+### Description
+
+Example creates a traditional *Box and Whiskers Chart* using *ChartXY*, *BoxSeries* and *PointSeries* for outliers.
+
+*Box and Whiskers Chart* provides a compact way of visually displaying distribution of data through *quartiles*.
+
+### Anatomy
+
+The lines extending parallel from the boxes are known as the "whiskers", which are used to indicate variability outside the upper and lower quartiles.
+
+*Outliers* are sometimes drawn as individual dots that are in-line with the whiskers.
+
+[//]: # "IMPORTANT: The assets will not show before README.md is built - relative path is different!"
+
+![](./assets/boxFigure.png)
+
+Here are the types of observations one can make from viewing a *Box And Whiskers Chart*:
+
+- What the key values are, such as: average, median, 25th percentile etc.
+- If there are any *outliers* and what their values are.
+- Is the data symmetrical.
+- How tightly is the data grouped.
+- If the data is skewed and if so, in what direction.
+
+### API links
+
+* XY cartesian chart: [ChartXY][]
+* Axis tick strategies: [AxisTickStrategies][]
+* AutoCursor modes: [AutoCursorModes][]
+* Point shapes: [PointShape][]
+* HEX Color factory: [ColorHEX][]
+* Solid Fill style: [SolidFill][]
+* Solid Line style: [SolidLine][]
+* Empty Axis tick style: [emptyTick][]
+* Empty Fill style: [emptyFill][]
+* Empty Line style: [emptyLine][]
+
+
+### Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+© Arction Ltd 2009-2019. All rights reserved.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+[AutoCursorModes]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/enums/autocursormodes.html
+[AxisTickStrategies]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#axistickstrategies
+[ChartXY]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/chartxy.html
+[ColorHEX]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#colorhex
+[emptyFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptyfill
+[emptyLine]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptyline
+[emptyTick]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptytick
+[PointShape]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/enums/pointshape.html
+[SolidFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidfill.html
+[SolidLine]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidline.html
---
+++ src/index.js
@@ -0,0 +1,195 @@
+/**
+ * LightningChartJS example that showcases the creation and styling of box series.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    emptyTick,
+    emptyFill,
+    emptyLine,
+    ColorHEX,
+    SolidFill,
+    SolidLine,
+    AxisTickStrategies,
+    AutoCursorModes,
+    PointShape
+} = lcjs
+
+// ----- Define data for application -----
+const allData = [
+    {
+        name: 'Software developer',
+        color: '#e67e22',
+        outlierShape: PointShape.Circle,
+        data: {
+            lowerExtreme: 17.5,
+            lowerQuartile: 19.6,
+            median: 21.2,
+            upperQuartile: 28.5,
+            upperExtreme: 48.1,
+            outliers: [
+                50.1
+            ]
+        }
+    },
+    {
+        name: 'Cashier',
+        color: '#2980b9',
+        outlierShape: PointShape.Circle,
+        data: {
+            lowerExtreme: 14.0,
+            lowerQuartile: 19.5,
+            median: 20.1,
+            upperQuartile: 26.7,
+            upperExtreme: 41.6,
+            outliers: [
+                52.2,
+                59.9
+            ]
+        }
+    },
+    {
+        name: 'Janitor',
+        color: '#27ae60',
+        outlierShape: PointShape.Circle,
+        data: {
+            lowerExtreme: 15.2,
+            lowerQuartile: 18.5,
+            median: 22.7,
+            upperQuartile: 34.1,
+            upperExtreme: 41.0,
+            outliers: []
+        }
+    }
+]
+
+// ----- Define styles for light theme -----
+const theme = {
+    whiteFill: new SolidFill({ color: ColorHEX('#FFFFFF') }),
+    lightGrayFill: new SolidFill({ color: ColorHEX('#A0A0A0A0') }),
+    darkFill: new SolidFill({ color: ColorHEX('#505050') })
+}
+
+// ----- Create a XY Chart -----
+const chart = lightningChart().ChartXY({
+    defaultAxisYTickStrategy: AxisTickStrategies.NumericWithUnits
+})
+    .setTitle('Age distribution across professions')
+    .setTitleFillStyle(theme.darkFill)
+    .setTitleFont((font) => font
+        .setSize(32)
+    )
+    // Disable interactions.
+    .setAutoCursorMode(AutoCursorModes.disabled)
+    .setMouseInteractions(false)
+
+// ----- Style Chart to a lighter theme -----
+chart
+    .setBackgroundFillStyle(theme.whiteFill)
+    .setChartBackgroundFillStyle(theme.whiteFill)
+
+// ----- Setup axes -----
+const gridStrokeStyle = new SolidLine({
+    thickness: 4,
+    fillStyle: theme.lightGrayFill
+})
+const axisX = chart.getDefaultAxisX()
+    .setTitle('Profession')
+    .setTitleFillStyle(theme.darkFill)
+    .setStrokeStyle(gridStrokeStyle)
+    // No default ticks.
+    .setTickStyle(emptyTick)
+    // Disable interactions.
+    .setMouseInteractions(false)
+
+const axisY = chart.getDefaultAxisY()
+    .setTitle('Age')
+    .setTitleFillStyle(theme.darkFill)
+    .setTickStyle((visibleTicks) => visibleTicks
+        .setLabelFillStyle(theme.darkFill)
+        .setTickStyle(emptyLine)
+        .setGridStrokeStyle(gridStrokeStyle)
+    )
+    .setStrokeStyle(gridStrokeStyle)
+    // Set Y-view manually.
+    .setScrollStrategy(undefined)
+    .setInterval(10, 63)
+    // Disable interactions.
+    .setMouseInteractions(false)
+
+// ----- Map over per each data item -----
+const boxFigureStrokeStyle = new SolidLine({
+    thickness: 4,
+    fillStyle: theme.darkFill
+})
+const outlierFillStyle = theme.darkFill
+
+allData.forEach((profession, i) => {
+    const data = profession.data
+    const fillStyle = new SolidFill({ color: ColorHEX(profession.color) })
+    // ----- Create series for rendering this data item -----
+    // Create BoxSeries.
+    const boxSeries = chart.addBoxSeries()
+        .setDefaultStyle((boxAndWhiskersFigure) => boxAndWhiskersFigure
+            .setBodyFillStyle(fillStyle)
+            .setBodyStrokeStyle(boxFigureStrokeStyle)
+            .setMedianStrokeStyle(boxFigureStrokeStyle)
+            .setStrokeStyle(boxFigureStrokeStyle)
+            .setBodyWidth(0.70)
+            .setTailWidth(0.70)
+        )
+
+    // Create PointSeries for outliers.
+    const pointSeries = chart.addPointSeries({
+        pointShape: profession.outlierShape ? profession.outlierShape : PointShape.Circle
+    })
+        .setPointSize(20)
+        .setPointFillStyle(outlierFillStyle)
+
+    // ----- Setup shared highlighting between box and point series -----
+    boxSeries.onHover((_, cp) => pointSeries.setHighlighted(cp !== undefined))
+    pointSeries.onHover((_, cp) => boxSeries.setHighlighted(cp !== undefined))
+
+    // ----- Compute X positions for BoxFigure -----
+    const start = i * 1
+    const end = start + 1
+    const middle = (start + end) / 2
+
+    // ----- Render BoxFigure -----
+    boxSeries.add({
+        start,
+        end,
+        lowerExtreme: data.lowerExtreme,
+        lowerQuartile: data.lowerQuartile,
+        median: data.median,
+        upperQuartile: data.upperQuartile,
+        upperExtreme: data.upperExtreme,
+    })
+
+    // ----- Render outliers -----
+    data.outliers.forEach((outlier) => {
+        pointSeries.add({
+            x: middle,
+            y: outlier
+        })
+    })
+
+    // ----- Create CustomTick on X-Axis for displaying name of profession -----
+    axisX.addCustomTick()
+        .setValue(middle)
+        .setTextFormatter(() => profession.name)
+        .setGridStrokeLength(0)
+        .setMarker((marker) => marker
+            .setTextFillStyle(theme.darkFill)
+            .setBackground((background) => background
+                .setFillStyle(emptyFill)
+                .setStrokeStyle(emptyLine)
+            )
+            .setFont((font) => font
+                .setSize(24)
+            )
+        )
+})
